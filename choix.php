caa9bd0922733cba637288178696fb5c7e277bc5
Niels34410 <chaigneau.niels@gmail.com>
2017-01-06 15:52:57 +0100

    Add files via upload

--- choix.php
+++ choix.php
@@ -1,3 +1,15 @@
 <?php
-
+//foreach($_POST['choix'] as $valeur){
+//echo '<p>',"La radio $valeur a été cochée<br>", '<section id=heure>',$heure =date("H:i"),'</section>','</p>';
+//fputs($fp, '<p>La radio $valeur a été cochée<br><section id=heure>$heure =date("H:i")</section></p>');
+//fputs($fp, $valeur);
+//}
+//foreach($_POST['texte'] as $value){
+//echo '<p>',"La box, $value a été écrite<br>", '<section id=heure>',$heure =date("H:i"),'</section>','</p>';
+//fputs($fp, '<p>La box $value a été écrite<br> <section id=heure>$heure =date("H:i")</section></p>');
+//  }
+foreach($_POST as $key => $value)
+{
+    echo $key . '<p>'," réponse : $value<br>",'</p>';
+}
 ?>
--- index.php
+++ index.php
@@ -3,45 +3,54 @@ $file = fopen("questions.qs", "r" );
 $lines=file("questions.qs");$theme = "##";
 $question = "#";
 $possibilites = array();
+$tableau= file("questions.qs");
 $numero=0;
 echo '<!DOCTYPE html>'.
-    '<html>'.
-    '<head>'.
-    '<meta http-equiv="content-type" content="text/html; charset=utf-8" />'.
-    '<title>Evaluation du 3 janvier 2017 – Béziers</title>'.
-    '<link rel="stylesheet" type="text/css" href="style.css">'.
-    '</head>'.
-    '<body>'.
-    '<article>'.
-    '<h1>Evaluation du 3 janvier 2017 – Béziers</h1>'.
-    '<h2>Niels Chaigneau</h2>'.
-    '<section id="bloc1">';
-    echo "\n";
+   '<html>'.
+   '<head>'.
+   '<meta http-equiv="content-type" content="text/html; charset=utf-8" />'.
+   '<title>Evaluations du 3 janvier 2017 – Béziers</title>'.
+   '<link rel="stylesheet" type="text/css" href="style.css">'.
+   '<link rel="javascript" type="text/javascript" href="javascript.js">'.
+   '</head>'.
+   '<body>'.
+   '<article>'.
+   '<h1>Evaluations du 3 janvier 2017 – Béziers</h1>'.
+   '<h2>Niels Chaigneau</h2>'.
+   '<section id="bloc1">';
+   echo "\n";
 foreach ($lines as $linenumber => $linecontent) {
-  if (empty($linecontent) or strlen($linecontent) < 2) continue;
-  if (substr($linecontent, 0, 2) == "##") {
-      // On reinitialise les variables quand on detecte un nouveau theme
-      $question = "";
-      $possibilites = array();
+ if (empty($linecontent) or strlen($linecontent) < 2) continue;
+ if (substr($linecontent, 0, 2) == "##") {
+     // On reinitialise les variables quand on detecte un nouveau theme
+     $question = "";
+     $possibilites = array();
 echo '</article><article>';
-      $tableau1 = $linecontent;
+     $tableau1 = $linecontent;
 echo '<h3>',$tableau1,'</h3>'."\n";
-  }
-  elseif ($linecontent[0] == '#' and $linecontent[1] != '#') {
-      // On reinitialise notre variable possibilité quand on detecte une nouvelle question
-      $possibilites = array();
-      $tableau2 = $linecontent;
+ }
+ elseif ($linecontent[0] == '#' and $linecontent[1] != '#') {
+     // On reinitialise notre variable possibilité quand on detecte une nouvelle question
+     $possibilites = array();
+     $tableau2 = $linecontent;
 echo '<section id=bloc1><h4><span>',$numero, $tableau2,'</span></h4>', "\n";
-      $numero ++;    }    elseif ($linecontent[0] == '-')
-  {
-    $possibilites = $linecontent;
+if ($tableau[$linenumber+1][0]== "#") {
+echo '<input type="textarea" name="'.$numero.'" value="'.$value.'">'.'<br>';
+//echo '<input type="text" name="texte">'.'<br>';
+}
+     $numero ++;    }    elseif ($linecontent[0] == '-')
+ {
+   $possibilites = $linecontent;
 // echo '<li>'.$possibilites.'</li>'."\n";
 echo '<form method="POST" action="choix.php">
-     <input type="checkbox" name="choix[]" value="'.$tableau2.$linecontent.'">'.$possibilites.'<br>';     // <input type="checkbox" name="choix[]" value="2"> nom 2<br>
-     // <input type="checkbox" name="choix[]" value="3"> nom 3<br>
-     // <input type="checkbox" name="choix[]" value="4"> nom 4<br>
-     // <input type="checkbox" name="choix[]" value="5"> nom 5<br>
-   }
- }
+    <input type="radio" name="'.$numero.'" value="'.$tableau2.$linecontent.'">'.$possibilites.'<br>';
+    //print"$str";
+    // <input type="checkbox" name="choix[]" value="2"> nom 2<br>
+    // <input type="checkbox" name="choix[]" value="3"> nom 3<br>
+    // <input type="checkbox" name="choix[]" value="4"> nom 4<br>
+    // <input type="checkbox" name="choix[]" value="5"> nom 5<br>
+  }
+}
 echo '<input type="submit" value="Envoyer">';
 echo '</form>';
+?>
